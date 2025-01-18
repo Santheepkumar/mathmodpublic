e04485756759709879a2b20f76acf7f64a8cbb97
Santheepkumar <santheephoney@gmail.com>
2025-01-19 00:54:19 +0530

    add mathmodpublic package

---
+++ mathmodpublic.go
@@ -0,0 +1,19 @@
+package mathmodpublic
+
+import "fmt"
+
+func Greet(name string) {
+	fmt.Printf("Welcome %s", name)
+}
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Multiply(x, y int) int {
+	return x * y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
